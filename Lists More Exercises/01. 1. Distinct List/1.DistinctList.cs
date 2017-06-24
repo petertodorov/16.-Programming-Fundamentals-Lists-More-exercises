4ce07a849e27f0b3cb828fa8cda57cc695c2279e
petertodorov <petodorov@gmail.com>
2017-06-24 17:04:35 +0300

    E 1

---
+++ Lists More Exercises/01. 1. Distinct List/1.DistinctList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class DistincList
+{
+    static void Main(string[] args)
+    {
+        List<int> input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        Console.WriteLine(String.Join(" ",input.Distinct()));
+    }
+}
+
